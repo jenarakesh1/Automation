549e1ace102a4146742f99e45550200566b1d21a
HP <HP@DESKTOP-V6DLRSM>
2024-06-26 16:15:43 +0530

    boss this is my 1st github account

---
+++ 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/Java_project/src/com/practice/Demo.java
@@ -0,0 +1,7 @@
+package com.practice;
+
+public class Demo {
+	public static void main(String []args) {
+
+ }
+}
---
+++ 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/Java_project/src/com/practice/Primitive.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class Primitive {
+public static void main(String[] args) {
+	int d=10;
+	double h=15.0;
+	long j=64;
+	char c='B';
+	System.out.println("Value d " + +d);
+	System.out.println("Value h "+ +h);
+	System.out.println("Value j "+ +j);
+	System.out.println("Value c "+ +c);
+	
+	
+}
+
+}
